1410bf602819796ee1d8eb612ccb525ae7871152
Deyht <david.cornu@utinam.cnrs.fr>
2019-12-20 16:02:50 +0100

    add timer.c

---
+++ exercices/cuda/timer.c
@@ -0,0 +1,14 @@
+void init_timing(struct timeval* tstart)
+{
+    gettimeofday(tstart, NULL);
+}
+
+float ellapsed_time(struct timeval tstart)
+{
+    struct timeval tmp;
+    long long diff;
+    gettimeofday(&tmp, NULL);
+    diff = tmp.tv_usec - tstart.tv_usec;
+    diff += (tmp.tv_sec - tstart.tv_sec) * 1000000;
+    return ((float)diff*1.0e-6);
+}
